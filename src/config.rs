68a12f1de29f62ea28c39b20440981fcdb399cea
bi4qaa <70623514+bi4qqaa@users.noreply.github.com>
2025-11-27 18:31:42 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -106,8 +106,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["58.210.43.14"];
+pub const RS_PUB_KEY: &str = "YUJ59NjU7edRqSykV4dHD7SS08yEwdKDT05Re7suIRE=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
